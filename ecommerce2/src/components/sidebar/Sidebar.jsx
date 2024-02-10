82f6a51567f69031dfda8d9d5ceb9e3e6c649642
Ojima <akbenngold@gmail.com>
2024-02-10 01:16:06 +0100

    done with the side bar fqactoring

--- ecommerce2/src/components/sidebar/Sidebar.jsx
+++ ecommerce2/src/components/sidebar/Sidebar.jsx
@@ -1,48 +1,96 @@
-import React, { Component } from "react";
-import "./Sidebar.css";
-import "../../styles/utils.css";
+import React from "react";
+import SidebarItem from "./SidebarItem";
 
-class Sidebar extends Component {
-  constructor() {
-    super();
-    this.state = {
-      icons: [
+function Sidebar() {
+  const icons = [
+    {
+      categoryTitle: "MAIN",
+      subcategories: [
         {
-          Overview: "../../assets/icons/overview.svg",
-          Analytics: "../../assets/icons/analytics.svg",
-          Ecommerce: "../../assets/icons/ecommerce.svg",
-          Crypto: "../../assets/icons/crypto.svg",
-          Account: "../../assets/icons/account.svg",
-          Customers: "../../assets/icons/customers.svg",
-          Products: "../../assets/icons/products.svg",
+          title: "Overview",
+          link: "src/assets/icons/overview.svg",
         },
         {
-          Orders: "../../assets/icons/orders.svg",
-          Invoices: "../../assets/icons/invoices.svg",
-          Logistics: "../../assets/icons/logistics.svg",
-          Academy: "../../assets/icons/academy.svg",
-          Job: "../../assets/icons/job.svg",
-          Social: "../../assets/icons/social.svg",
-          Blog: "../../assets/icons/blog.svg",
-          File: "../../assets/icons/file.svg",
-          Kanban: "../../assets/icons/kanban.svg",
-          Mail: "../../assets/icons/mail.svg",
-          Chat: "../../assets/icons/chat.svg",
-          Calendar: "../../assets/icons/calendar.svg",
+          title: "Analytics ",
+          link: "src/assets/icons/analytics.svg",
+        },
+        {
+          title: "Ecommerce ",
+          link: "src/assets/icons/ecommerce.svg",
+        },
+
+        {
+          title: "Crypto",
+          link: "src/assets/icons/crypto.svg",
+        },
+
+        {
+          title: "Accounts ",
+          link: "src/assets/icons/overview.svg",
+        },
+        {
+          title: "Customers ",
+          link: "src/assets/icons/customers.svg",
+        },
+        {
+          title: "Products",
+          link: "src/assets/icons/products.svg",
         },
       ],
-    };
-  }
-
-  render() {
-    return (
-      <>
-        {this.state.icons.map((iconGroup) => {
-          return console.log(iconGroup);
-        })}
-      </>
-    );
-  }
+    },
+    {
+      categoryTitle: "CONCEPT",
+      subcategories: [
+        {
+          title: "Orders",
+          link: "src/assets/icons/orders.svg",
+        },
+        {
+          title: "Invoices",
+          link: "src/assets/icons/invoices.svg",
+        },
+        {
+          title: "Logistics",
+          link: "src/assets/icons/logistics.svg",
+        },
+        {
+          title: "Academy",
+          link: "src/assets/icons/academy.svg",
+        },
+        {
+          title: "Job",
+          link: "src/assets/icons/job.svg",
+        },
+        { title: "Social", link: "src/assets/icons/social.svg" },
+        { title: "Blog", link: "src/assets/icons/blog.svg" },
+        { title: "File", link: "src/assets/icons/file.svg" },
+        { title: "Kanban", link: "src/assets/icons/kanban.svg" },
+        { title: "Mail", link: "src/assets/icons/mail.svg" },
+        { title: "Chat", link: "src/assets/icons/link.svg" },
+        { title: "Calendar", link: "src/assets/icons/calendar.svg" },
+      ],
+    },
+  ];
+  return (
+    <div>
+      {icons.map((icon) => {
+        return (
+          <div key={icon.categoryTitle}>
+            {icon.categoryTitle}
+            {icon.subcategories.map((item, index) => {
+              return (
+                <SidebarItem
+                  key={index}
+                  title={item.title}
+                  iconSvg={item.link}
+                />
+              );
+            })}
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Sidebar;
--- ecommerce2/src/components/sidebar/SidebarItem.jsx
+++ ecommerce2/src/components/sidebar/SidebarItem.jsx
@@ -3,10 +3,18 @@ import React, { Component } from "react";
 class SidebarItem extends Component {
   constructor() {
     super();
-    state = {};
   }
   render() {
-    return <div>{/* {this.props.icon} {this.props.title} */}</div>;
+    return (
+      <div className="sidebar">
+        <div className="sidebar__item">
+          <div className="sidebar__icon">
+            <img src={this.props.iconSvg} className="sidebar__icon" alt="" />
+          </div>
+          <div className="sidebar__item__title">{this.props.title}</div>
+        </div>
+      </div>
+    );
   }
 }
 
